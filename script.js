654096268365099928a05fd98b7789a4b87757e2
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-17 01:08:22 +0200

    Add function to generate the squared canvas

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const canvasContainer = document.getElementById('sketch');
+
+
+function createRows (container,num) {
+    for (let i = num; i > 0; i--) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+    }
+}
+
+function createSquares (container,num) {
+    for (let i = num; i > 0; i--) {
+        const square = document.createElement('div');
+        square.classList.add('canvas-square');
+        container.appendChild(square);
+    }
+}
+
+function createCanvas(num) {
+    createRows(canvasContainer,num);
+    const rowsCreated = document.getElementsByClassName('row');
+    for(let i = 0; i < rowsCreated.length; i++) {
+        createSquares(rowsCreated[i], num);
+    }        
+ }
+
+createCanvas(16);
