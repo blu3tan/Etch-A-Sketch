2862586f7934d7cd5b67cce8b05624756bd2c937
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-21 22:29:37 +0200

    Refactor drawing logic to allow random color

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ const inputBox = document.getElementById('inputBox');
 const resetButton = document.getElementById('resetBtn');
 const eraseButton = document.getElementById('eraseBtn');
 const blackButton = document.getElementById('blackBtn');
+const rainbowButton = document.getElementById('rainbowBtn');
 const message = document.getElementById('messageText');
 const square = document.getElementsByClassName('canvas-square')
 
 let isMousedown = false;
-const colors = ['black', 'erase'];
-let selector = 0;
+let color = 'black';
+
 
 inputBox.addEventListener('input', () => {
     grid = inputBox.value;
 })
 
 eraseButton.addEventListener('click', () => {
-    selector = 1;
+    color = 'white';
 })
 
 blackButton.addEventListener('click', () => {
-    selector = 0;
+    color = 'black';
+})
+
+rainbowButton.addEventListener('click', () => {
+    color = 'pink';
 })
 
 // Catch mouseup event outside the canvas
@@ -53,7 +58,7 @@ function clearCanvas(element) {
     }
 }
 
-// Canvas generation //
+// Canvas generation  functions//
 
 function createRows (container,num) {
     for (let i = num; i > 0; i--) {
@@ -84,20 +89,11 @@ function createCanvas(num) {
 // Initialization of the drawing mechanism
 
 function initDraw (element) {
-    element.addEventListener('mousedown', () => {
-        isMousedown = true;
-        clearClasses(element);
-        element.classList.add(colors[selector]);
-    });
+    element.addEventListener('mousedown', changeColor);
     element.addEventListener('mouseup', () => {
         isMousedown = false;
     });
-    element.addEventListener('mousemove', () => {
-        if (isMousedown) {
-            clearClasses(element);
-            element.classList.add(colors[selector]);
-        }
-    });
+    element.addEventListener('mousemove', changeColorMove);
 }
 
 function clearClasses (element) {
@@ -106,5 +102,21 @@ function clearClasses (element) {
     }
 }
 
+function randomColor() {
+    let hexColor = '#' + (Math.random().toString(16) + "000000").substring(2,8).toUpperCase();
+    return hexColor;
+}
+
+function changeColor () {
+    isMousedown = true;
+    this.style.backgroundColor = color;
+} 
+function changeColorMove () {
+    if (isMousedown) {
+        this.style.backgroundColor = color;
+        // Prevents the conflict with the browser drag behaviour
+        element.ondragstart = () => false;
+    }
+} 
 
 createCanvas(16);
